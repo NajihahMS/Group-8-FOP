bb2935619fae3df5a30841a5b9e6b07484b6de17
Nabilah Mohd Sallehuddin <24001501@siswa.um.edu.my>
2026-01-08 14:52:53 +0000

    hi

--- DataClass/Model.java
+++ DataClass/Model.java
@@ -18,7 +18,25 @@ public class Model{
 
     public String getName() { return modelName; }
     public double getPrice() { return price; }
-    
+    // FIX: Returns all outlet stocks as an array for the StockManagement class to read
+    public int[] getStocks() {
+        return new int[] {c60, c61, c62, c63, c64, c65, c66, c67, c68, c69};
+    }
+
+    public void setStock(int index, int newQuantity) {
+        switch(index) {
+            case 0: this.c60 = newQuantity; break;
+            case 1: this.c61 = newQuantity; break;
+            case 2: this.c62 = newQuantity; break;
+            case 3: this.c63 = newQuantity; break;
+            case 4: this.c64 = newQuantity; break;
+            case 5: this.c65 = newQuantity; break;
+            case 6: this.c66 = newQuantity; break;
+            case 7: this.c67 = newQuantity; break;
+            case 8: this.c68 = newQuantity; break;
+            case 9: this.c69 = newQuantity; break;
+        }
+    }
     // Crucial for saving later
     public String toCSV() {
         return modelName + "," + price + "," + c60 + "," + c61 + "," + c62 + 
