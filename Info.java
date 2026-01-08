6f9c8c8cc17cf2f0388c4e9fae9309bed1ce98f8
aqilah-2025 <24002437@siswa.um.edu.my>
2026-01-08 14:37:57 +0000

    add info.java parent class

--- Info.java
+++ Info.java
@@ -1,1 +1,16 @@
-I
+package GoldenHour; 
+
+/**
+ * Polymorphism
+ * - parent type
+ * - SearchInfo and EditInfo EXTEND this.
+ * - OVERRIDE run() with different behavior.
+ */
+public abstract class Info {
+
+    // This method will be overridden by child classes
+    // call run() using InfoModule reference.
+    public abstract void run();
+}
+
+
